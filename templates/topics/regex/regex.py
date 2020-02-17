b928d29fa6097a94e70dd627055e19dcae148bb6
Alfred-Walker <studio.alfred.walker@gmail.com>
2020-02-17 12:42:39 +0900

    [Add]: re.IGNORECASE example [Misc]: changed print result comments

--- templates/topics/regex/regex.py
+++ templates/topics/regex/regex.py
@@ -11,12 +11,13 @@ class RegexExample:
     @staticmethod
     def re_findall():
         """findall() finds compiled regular expression from the text in args"""
-        text = "__example__text__example"
-        regex = re.compile("example")
+        text = "__example__text__Example"
+        regex = re.compile("example", re.IGNORECASE)
         result = regex.findall(text)
 
         # all match results will be printed
-        print(result)   # ['example', 'example']
+        print(result)
+        # >>> ['example', 'Example']
 
     @staticmethod
     def re_search():
@@ -26,10 +27,12 @@ class RegexExample:
         match_object = regex.search(text)
 
         # return type is Match object
-        print(type(match_object))     # <class 're.Match'>
+        print(type(match_object))
+        # >>> <class 're.Match'>
 
         # print subgroups of the 'match object'
-        print(match_object.group())   # example
+        print(match_object.group())
+        # >>> example
 
     @staticmethod
     def decimal_pattern():
@@ -37,7 +40,8 @@ class RegexExample:
         text = "Phone number: 032-232-3245"
         regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
         match_object = regex.search(text)
-        print(match_object.group())     # 032-232-3245
+        print(match_object.group())
+        # >>> 032-232-3245
 
     @staticmethod
     def start_char_pattern():
@@ -45,11 +49,13 @@ class RegexExample:
         text = "start_with"
         regex = re.compile(r"^art")
         result = regex.findall(text)
-        print(result)       # [] (not found)
+        print(result)
+        # >>> [] (not found)
 
         regex = re.compile(r"^start")
         result = regex.findall(text)
-        print(result)  # ['start']
+        print(result)
+        # >>> ['start']
 
     @staticmethod
     def end_char_pattern():
@@ -57,11 +63,13 @@ class RegexExample:
         text = "end_with"
         regex = re.compile(r"wit$")
         result = regex.findall(text)
-        print(result)       # [] (not found)
+        print(result)
+        # >>> [] (not found)
 
         regex = re.compile(r"with$")
         result = regex.findall(text)
-        print(result)  # ['with']
+        print(result)
+        # >>> ['with']
 
     @staticmethod
     def greedy_qualifier_pattern():
@@ -69,7 +77,8 @@ class RegexExample:
         text = "start_end"
         regex = re.compile(r"art.*")
         result = regex.findall(text)
-        print(result)  # ['art_end']
+        print(result)
+        # >>> ['art_end']
 
     @staticmethod
     def decimal_range_pattern():
@@ -77,7 +86,8 @@ class RegexExample:
         text = "ab12ab3456"
         pattern = r'ab[1-3]'  # split by ab1, ab3
         result = re.split(pattern, text)
-        print(result)  # ['', '2', '456']
+        print(result)
+        # >>> ['', '2', '456']
 
     @staticmethod
     def range_and_count_pattern():
@@ -92,6 +102,7 @@ class RegexExample:
         regex = re.compile(target)
         result = regex.findall(text)
         print(result)
+        # >>> [('010', '12312-1111')]
 
     @staticmethod
     def split():
@@ -99,7 +110,8 @@ class RegexExample:
         text = "___split1||||split2____split3||||"
         pattern = r'split[1-3]'
         result = re.split(pattern, text)
-        print(result)  # ['___', '||||', '____', '||||']
+        print(result)
+        # >>> ['___', '||||', '____', '||||']
 
     @staticmethod
     def text_with_ranged_decimal_pattern():
@@ -107,7 +119,8 @@ class RegexExample:
         text = "ab12ab3456"
         pattern = r'ab[1-3]'  # split by ab1, ab3
         result = re.split(pattern, text)
-        print(result)  # ['', '2', '456']
+        print(result)
+        # >>> ['', '2', '456']
 
 
 RegexExample.re_findall()
